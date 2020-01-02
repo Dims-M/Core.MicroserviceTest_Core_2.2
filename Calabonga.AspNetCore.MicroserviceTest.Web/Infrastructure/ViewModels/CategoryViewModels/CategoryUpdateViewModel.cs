b91d3c218f201a808168a62db44769006228a5f4
Дмитрий <rabolan@mail.ru>
2020-01-02 22:51:00 +0300

    yt dehsxbn

---
+++ Calabonga.AspNetCore.MicroserviceTest.Web/Infrastructure/ViewModels/CategoryViewModels/CategoryUpdateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Calabonga.AspNetCore.MicroserviceTest.Web.Infrastructure.Factories.Base;
+
+namespace Calabonga.AspNetCore.MicroserviceTest.Web.Infrastructure.ViewModels.CategoryViewModels
+{
+    public class CategoryUpdateViewModel : IViewModel
+    {
+    }
+}
---
+++ Calabonga.AspNetCore.MicroserviceTest.Web/Infrastructure/ViewModels/CategoryViewModels/CategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Calabonga.AspNetCore.MicroserviceTest.Web.Infrastructure.Factories.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calabonga.AspNetCore.MicroserviceTest.Web.Infrastructure.ViewModels.CategoryViewModels
+{
+    public class CategoryViewModel : IViewModel
+    {
+
+    }
+}
